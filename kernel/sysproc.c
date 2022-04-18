a1fecfe46d5ef70b0f12fcef3cabf9501b375401
ysyx_22040937-Xie Cheng <2960252562@qq.com>
2022-04-18 10:48:51 +0800

    Eliminate allocation from sbrk() (easy)

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -41,15 +41,16 @@ sys_wait(void)
 uint64
 sys_sbrk(void)
 {
-  int addr;
+  int oldaddr;
   int n;
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
-  return addr;
+  oldaddr = myproc()->sz;
+  myproc()->sz+=n;
+  //if(growproc(n) < 0)
+  //  return -1;
+  return oldaddr;
 }
 
 uint64
